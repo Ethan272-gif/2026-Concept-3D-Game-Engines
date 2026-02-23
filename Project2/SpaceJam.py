f64187b6ee11693803468ea35c9c0e002ccdada8
Ethan272-gif <eschwab5@student.cscc.edu>
2026-02-23 15:31:02 -0500

    hi

--- Project2/SpaceJam.py
+++ Project2/SpaceJam.py
@@ -9,23 +9,23 @@ class SpaceJam(ShowBase):
         ShowBase.__init__(self)
         self.accept('escape',self.quit)
         self.Universe=SpaceClass.Universe(
-            self.loader, "./assets/Universe/Universe.x", self.render, 'Universe',"./assets/Universe/Universe.jpg",(0,0,0),15000)
+            self.loader, "./assets/Universe/Universe.x", self.render, 'Universe',"./assets/Universe/Universe.jpg",(0,0,0),15000)# type: ignore
         self.Hero=SpaceClass.SpaceShip(
-            self.loader,"./assets/Space_Ship/Dumbledore/Dumbledore.x", self.render,'Hero',"./assets/Space_Ship/Dumbledore/spacejet_C.png",(450,5000,-500),100)
+            self.loader,"./assets/Space_Ship/Dumbledore/Dumbledore.x", self.render,'Hero',"./assets/Space_Ship/Dumbledore/spacejet_C.png",(450,5000,-500),100)# type: ignore
         self.SpaceStation=SpaceClass.SpaceStation(
-            self.loader,"./assets/Space_Station/SpaceStation1B/spaceStation.x",self.render,'Space Station',"./assets/Space_Station/SpaceStation1B/SpaceStation1_Dif2.png",(150,4000,-600),10)
+            self.loader,"./assets/Space_Station/SpaceStation1B/spaceStation.x",self.render,'Space Station',"./assets/Space_Station/SpaceStation1B/SpaceStation1_Dif2.png",(150,4000,-600),10)# type: ignore
         self.Planet1=SpaceClass.Planet(
-            self.loader,"./assets/Planets/protoPlanet.x",self.render,'Planet1',"./assets/Planets/Planet1/maps/Green_Planet.tif",(150,5000,67),350)# texture of Planet1 by Rebecca Deutsch 
+            self.loader,"./assets/Planets/protoPlanet.x",self.render,'Planet1',"./assets/Planets/Planet1/maps/Green_Planet.tif",(150,5000,67),350)# texture of Planet1 by Rebecca Deutsch # type: ignore
         self.Planet2=SpaceClass.Planet(
-            self.loader,"./assets/Planets/protoPlanet.x",self.render,'Planet2',"./assets/Planets/Planet2/maps/Desert_Planet.tif",(850,4000,67),250)#Texture Found Alice Gallery no moduler
+            self.loader,"./assets/Planets/protoPlanet.x",self.render,'Planet2',"./assets/Planets/Planet2/maps/Desert_Planet.tif",(850,4000,67),250)#Texture Found Alice Gallery no moduler# type: ignore
         self.Planet3=SpaceClass.Planet(
-            self.loader,"./assets/Planets/protoPlanet.x",self.render,'Planet3',"./assets/Planets/Planet3/maps/Fire_Planet.tif",(500,4000,700),450)#Texture by Bob Rost
+            self.loader,"./assets/Planets/protoPlanet.x",self.render,'Planet3',"./assets/Planets/Planet3/maps/Fire_Planet.tif",(500,4000,700),450)#Texture by Bob Rost# type: ignore
         self.Planet4=SpaceClass.Planet(
-            self.loader,"./assets/Planets/protoPlanet.x",self.render,'Planet4',"./assets/Planets/Planet4/maps/Sky_planet.tif",(-500,6000,700),550)#Texture by Jon Amkawa
+            self.loader,"./assets/Planets/protoPlanet.x",self.render,'Planet4',"./assets/Planets/Planet4/maps/Sky_planet.tif",(-500,6000,700),550)#Texture by Jon Amkawa# type: ignore
         self.Planet5=SpaceClass.Planet(
-            self.loader,"./assets/Planets/protoPlanet.x",self.render, 'Planet5',"./assets/Planets/Planet5/maps/Peach_Planet.tif",(-500,4000,300),200)#Texture by Justin Hsu/Jicjen Zhu
+            self.loader,"./assets/Planets/protoPlanet.x",self.render, 'Planet5',"./assets/Planets/Planet5/maps/Peach_Planet.tif",(-500,4000,300),200)#Texture by Justin Hsu/Jicjen Zhu# type: ignore
         self.Planet6=SpaceClass.Planet(
-            self.loader,"./assets/Planets/protoPlanet.x",self.render,'Planet6',"./assets/Planets/Planet6/maps/Hole_planet.tif",(0,2000,300),100)#Texture by Amy Ip
+            self.loader,"./assets/Planets/protoPlanet.x",self.render,'Planet6',"./assets/Planets/Planet6/maps/Hole_planet.tif",(0,2000,300),100)#Texture by Amy Ip# type: ignore
 
 
         fullCycle=60
@@ -43,33 +43,33 @@ class SpaceJam(ShowBase):
         unitVec.normalize()
         position=unitVec*radius*250+centralObject.modelNode.getPos()
         SpaceClass.Drone(
-            self.loader, "./assets/DroneDefender/DroneDefender.obj",self.render,droneName,"./assets/DroneDefender/octotoad1_auv.png", position,5)
+            self.loader, "./assets/DroneDefender/DroneDefender.obj",self.render,droneName,"./assets/DroneDefender/octotoad1_auv.png", position,5)# type: ignore
 
     def DrawCloudDefense(self, centralObject, droneName):
         unitVec=defensePaths.Cloud()
         unitVec.normalize()
         position=unitVec*500+centralObject.modelNode.getPos()
         SpaceClass.Drone(
-            self.loader, "./assets/DroneDefender/DroneDefender.obj",self.render,droneName,"./assets/DroneDefender/octotoad1_auv.png", position,10)
+            self.loader, "./assets/DroneDefender/DroneDefender.obj",self.render,droneName,"./assets/DroneDefender/octotoad1_auv.png", position,10)# type: ignore
         
     def DrawXPlainDefense(self, centralObject, droneName, step, totalSteps, radius=1):
         unitVec=defensePaths.x_plain(step, totalSteps)
         unitVec.normalize()
         position=unitVec*radius*300+centralObject.modelNode.getPos()
         SpaceClass.Drone(
-            self.loader, "./assets/DroneDefender/DroneDefender.obj",self.render,droneName,"./assets/DroneDefender/octotoad1_auv.png", position,10)
+            self.loader, "./assets/DroneDefender/DroneDefender.obj",self.render,droneName,"./assets/DroneDefender/octotoad1_auv.png", position,10)# type: ignore
     def DrawYPlainDefense(self, centralObject, droneName, step, totalSteps, radius=1):
         unitVec=defensePaths.y_plain(step, totalSteps)
         unitVec.normalize()
         position=unitVec*radius*300+centralObject.modelNode.getPos()
         SpaceClass.Drone(
-            self.loader, "./assets/DroneDefender/DroneDefender.obj",self.render,droneName,"./assets/DroneDefender/octotoad1_auv.png", position,10)
+            self.loader, "./assets/DroneDefender/DroneDefender.obj",self.render,droneName,"./assets/DroneDefender/octotoad1_auv.png", position,10)# type: ignore
     def DrawZPlainDefense(self, centralObject, droneName, step, totalSteps, radius=1):
         unitVec=defensePaths.z_plain(step, totalSteps)
         unitVec.normalize()
         position=unitVec*radius*300+centralObject.modelNode.getPos()
         SpaceClass.Drone(
-            self.loader, "./assets/DroneDefender/DroneDefender.obj",self.render,droneName,"./assets/DroneDefender/octotoad1_auv.png", position,10)
+            self.loader, "./assets/DroneDefender/DroneDefender.obj",self.render,droneName,"./assets/DroneDefender/octotoad1_auv.png", position,10)# type: ignore
         
         
 
